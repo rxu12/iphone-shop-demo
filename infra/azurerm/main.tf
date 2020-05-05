dc1b110645a92fab36473647f0b14a046a35cc9c
Rui Xu <rxuofficeworks@gmail.com>
2020-05-06 00:57:50 +1000

    loosen azure plugin version

--- infra/azurerm/main.tf
+++ infra/azurerm/main.tf
@@ -1,11 +1,11 @@
 # The Azure Active Resource Manager Terraform provider
 provider "azurerm" {
-  version = "=1.36.0"
+  version = "~>1.36.0"
 }
 
 # The Azure Active Directory Terraform provider
 provider "azuread" {
-  version = "=0.6.0"
+  version = "~>0.6.0"
 }
 
 # Reference to the current subscription.  Used when creating role assignments
