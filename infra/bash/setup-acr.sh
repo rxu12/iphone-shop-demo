2bd70042d44aefd809f0c07f5f5c40d7d965de3b
Rui Xu <rxuofficeworks@gmail.com>
2020-05-06 23:38:34 +1000

    auto approve

--- infra/bash/setup-acr.sh
+++ infra/bash/setup-acr.sh
@@ -4,5 +4,5 @@ export ARM_CLIENT_SECRET=$AZURE_CLIENT_SECRET
 export ARM_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID
 export ARM_TENANT_ID=$AZURE_TENANT_ID
 cd ./azurerm
-terraform init -upgrade
-terraform apply
+terraform init -input=false -upgrade
+terraform apply -input=false -auto-approve
