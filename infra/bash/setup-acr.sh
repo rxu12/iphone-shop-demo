571379a3d8a2fe597d10aa7a10aa077cbe12bacd
Rui Xu <rxuofficeworks@gmail.com>
2020-05-06 22:42:49 +1000

    fix env vir prefix

--- infra/bash/setup-acr.sh
+++ infra/bash/setup-acr.sh
@@ -1,4 +1,8 @@
-az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET -t $TENANT_ID
+echo "My client id is $AZURE_CLIENT_ID"
+echo "My client secret is $AZURE_CLIENT_SECRET"
+echo "My tenant id is $AZURE_TENANT_ID"
+echo "My subscription id is $AZURE_SUBSCRIPTION_ID"
+az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
 cd ./azurerm
 terraform init -upgrade
 terraform apply
