792b6aa2c6ff34b2c660ff47d716ec8f44fe9377
Rui Xu <rxu@officeworks.com.au>
2020-05-08 21:18:10 +1000

    print azure sp client id

--- infra/bash/setup-acr.sh
+++ infra/bash/setup-acr.sh
@@ -3,6 +3,8 @@ export ARM_CLIENT_ID=$AZURE_CLIENT_ID
 export ARM_CLIENT_SECRET=$AZURE_CLIENT_SECRET
 export ARM_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID
 export ARM_TENANT_ID=$AZURE_TENANT_ID
+echo "My client id is $ARM_CLIENT_ID"
+
 cd ./azurerm
 terraform init -input=false -upgrade
 terraform apply -input=false -auto-approve
