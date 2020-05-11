12b8aff2685027e9e203aa0666edc1c18e9aeeea
Rui Xu <rxu@officeworks.com.au>
2020-05-11 23:44:00 +1000

    alpha numeric only

--- infra/azurerm/acr.tf
+++ infra/azurerm/acr.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "default" {
-  name                     = "${replace(var.app_name, "-", "")}_acr"
+  name                     = "${replace(var.app_name, "-", "")}acr"
   resource_group_name      = azurerm_resource_group.default.name
   location                 = azurerm_resource_group.default.location
   sku                      = "Standard"
