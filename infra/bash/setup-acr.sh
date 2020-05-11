d4d78ff0050b60dfdc5fe431c537fa63e6d24b99
Rui Xu <rxu@officeworks.com.au>
2020-05-11 21:58:37 +1000

    change TF log level

--- infra/bash/setup-acr.sh
+++ infra/bash/setup-acr.sh
@@ -3,8 +3,6 @@ export ARM_CLIENT_ID=$AZURE_CLIENT_ID
 export ARM_CLIENT_SECRET=$AZURE_CLIENT_SECRET
 export ARM_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID
 export ARM_TENANT_ID=$AZURE_TENANT_ID
-export ___A=$ARM_CLIENT_ID
-echo "My client id is $___A"
 
 cd ./azurerm
 terraform init -input=false -upgrade
