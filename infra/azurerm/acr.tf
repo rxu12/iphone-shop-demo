bcb2baa52b52c996a66f672c5f46064e274c26ae
Rui Xu <rxu@officeworks.com.au>
2020-05-11 23:42:15 +1000

    fix string concat

--- infra/azurerm/acr.tf
+++ infra/azurerm/acr.tf
@@ -1,8 +1,5 @@
-locals {
-  acr_name = "${replace(var.app_name, "-", "")}_acr"
-}
 resource "azurerm_container_registry" "default" {
-  name                     = local.acr_name
+  name                     = "${replace(var.app_name, "-", "")}_acr"
   resource_group_name      = azurerm_resource_group.default.name
   location                 = azurerm_resource_group.default.location
   sku                      = "Standard"
