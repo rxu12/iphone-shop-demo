aa8a739b8b4cecd76d89e8958fc1a5300d5c4ba1
Rui Xu <rxu@officeworks.com.au>
2020-05-11 21:20:19 +1000

    Lock up terraform provider version

--- infra/azurerm/main.tf
+++ infra/azurerm/main.tf
@@ -1,10 +1,17 @@
 # The Azure Active Resource Manager Terraform provider
 provider "azurerm" {
+  version = "~> 2.9"
   features {}
 }
 
 # The Azure Active Directory Terraform provider
-provider "azuread" {}
+provider "azuread" {
+  version = "~> 0.8"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
 
 # Reference to the current subscription.  Used when creating role assignments
 data "azurerm_subscription" "current" {}
