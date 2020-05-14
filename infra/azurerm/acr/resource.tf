dae061b737e6776216a54142757f110899599340
Rui Xu <rxu@officeworks.com.au>
2020-05-15 00:02:58 +1000

    reference rg

--- infra/azurerm/acr/resource.tf
+++ infra/azurerm/acr/resource.tf
@@ -4,16 +4,26 @@ provider "azurerm" {
   features {}
 }
 
+variable "app_name" {
+  type        = string
+  description = "Location of the azure resource group."
+  default     = "iphone-search"
+}
+
 variable "app_acr_name" {
   type        = string
   description = "name of the azure container registry."
   default     = "iphone-search"
 }
 
+data "azurerm_resource_group" "default" {
+  name = "${var.app_name}-rg"
+}
+
 resource "azurerm_container_registry" "default" {
   name                     = var.app_acr_name
-  resource_group_name      = azurerm_resource_group.default.name
-  location                 = azurerm_resource_group.default.location
+  resource_group_name      = data.azurerm_resource_group.default.name
+  location                 = data.azurerm_resource_group.default.location
   sku                      = "Standard"
   admin_enabled            = false
 }
