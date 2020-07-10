cfda967a00136e69dd553e88cd33296544dc8334
Rui Xu <rxu@192-168-1-106.tpgi.com.au>
2020-07-10 20:35:14 +1000

    test kv secret injection

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/CCP", function (req, res) {
+  res.json({ CCP_Client_Secret: process.env.CCP_Client_Secret });
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.WEBSITES_PORT || 9000);
